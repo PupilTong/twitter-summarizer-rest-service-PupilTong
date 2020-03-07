6de7aa83b847a6660401cf247fd2315fbb5a7766
PupilTong <earth.diqiu@gmail.com>
2020-03-06 22:52:27 -0500

    Update rest.py

--- rest.py
+++ rest.py
@@ -7,7 +7,7 @@ import json
 from ffmpegQueue import *
 
 app = Flask(__name__)
-app.appurl = 'https://hw5.onic.xyz/'
+app.appurl = 'https://127.0.0.1:5000/'
 
 
 @app.route('/status/<string:uuid>',methods=['GET'])
@@ -35,8 +35,8 @@ def GetVideo(uuid):
 
 @app.route('/',methods=['POST'])
 def Post():
+    r={}
     try:
-        r={}
         if not request.json or not 'keyword' in request.json:
             r['error']='illegal request!'
         else:
@@ -61,4 +61,4 @@ if __name__ == '__main__':
     access_token = conf.get('auth', 'access_token')
     access_token_secret = conf.get('auth', 'access_secret')
     app.videoApi = TwitterVideoSum(consumer_key, consumer_secret, access_token, access_token_secret)
-    app.run(debug = True,host='0.0.0.0',port = 6666)
+    app.run(debug = True,host='127.0.0.1',port = 5000)
